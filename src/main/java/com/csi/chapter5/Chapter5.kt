b163094947b33de680f51d22a0fe0c464c541458
Robinhood <haveahopes@gmail.com>
2021-03-15 23:49:02 +0900

    Chapter5

--- src/main/java/com/csi/chapter5/Chapter5.kt
+++ src/main/java/com/csi/chapter5/Chapter5.kt
@@ -1,5 +1,12 @@
 package com.csi.chapter5
 
 fun refineText(txt : String) : String{
-    return txt.replace("    ", " ").replace("   ", " ").replace("  ", " ")
+    return txt
+        .replace("    ", " ")
+        .replace("\t", " ")
+        .replace("  ", " ")
+        .replace("  ", " ")
+        .replace("  ", " ")
+        .replace("mockist", "*******")
+        .replace("purist", "******")
 }
--- src/test/java/com/csi/chapter5/RefineTest.kt
+++ src/test/java/com/csi/chapter5/RefineTest.kt
@@ -1,7 +1,9 @@
 package com.csi.chapter5
 
+import io.github.serpro69.kfaker.Faker
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
@@ -26,6 +28,28 @@ class RefineTest {
         Assertions.assertEquals(expected, actual)
     }
 
+    @ParameterizedTest(name = "{displayName}{index} {arguments}")
+    @DisplayName("bannedWordReplaceCharacter")
+    @MethodSource("provideBannedWordsHelloWorldStrings")
+    fun bannedWordReplaceCharacter(input: String, bannedWords : Array<String>, expected: String) {
+        val actual = refineText(input)
+        Assertions.assertEquals(expected, actual)
+    }
+
+    @Test
+    @DisplayName("givenBannedWordReplaceCharacter")
+    fun givenBannedWordReplaceCharacter() {
+        //Given
+        val bannedWord = Faker().lorem.words()
+        val source = "hello $bannedWord"
+
+        //When
+        val actual = refineText(source)
+
+        val expected = "hello ${"*".repeat(bannedWord.length)}"
+        Assertions.assertEquals(expected, actual)
+    }
+
     companion object {
         @JvmStatic
         fun provideSpaceHelloWorldStrings(): Stream<Arguments> {
@@ -42,7 +66,16 @@ class RefineTest {
         @JvmStatic
         fun provideTabHelloWorldStrings(): Stream<Arguments> {
             return Stream.of(
-                Arguments.of("hello\t world", "hello world")
+                Arguments.of("hello\t world", "hello world"),
+                Arguments.of("hello \tworld", "hello world")
+            )
+        }
+
+        @JvmStatic
+        fun provideBannedWordsHelloWorldStrings(): Stream<Arguments> {
+            return Stream.of(
+                Arguments.of("hello mockist", arrayOf("mockist", "purist"), "hello *******"),
+                Arguments.of("hello purist", arrayOf("mockist", "purist"), "hello ******")
             )
         }
     }
