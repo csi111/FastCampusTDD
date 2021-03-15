e5a532340070f8f424d0b951c97bfb0f82aa6b21
Robinhood <haveahopes@gmail.com>
2021-03-16 00:31:57 +0900

    Chapter6

--- src/main/java/com/csi/chapter6/Statistics.kt
+++ src/main/java/com/csi/chapter6/Statistics.kt
@@ -3,27 +3,37 @@ package com.csi.chapter6
 class Statistics {
 
     fun variance(`data's`: IntArray): String {
-        if (`data's`.isEmpty()) {
-            return "입력된 데이터가 없습니다."
+        return when {
+            `data's`.isEmpty() -> {
+                "입력된 데이터가 없습니다."
+            }
+            `data's`.size == 1 -> {
+                "데이터가 부족해 분산을 계산할 수 없습니다. 2개 이상의 데이터를 입력해 주세요."
+            }
+            else -> {
+                val mean = calculateMean(`data's`)
+
+                val sumOfSquares = calculateSumOfSquares(`data's`, mean)
+
+                val variance = sumOfSquares / (`data's`.size - 1)
+
+                "분산 : $variance"
+            }
         }
+    }
 
-        if (`data's`.size == 1) {
-            return "데이터가 부족해 분산을 계산할 수 없습니다. 2개 이상의 데이터를 입력해 주세요."
-        }
-
-        val sum = `data's`.sum()
-        val mean: Double = (sum / `data's`.size).toDouble()
-
+    private fun calculateSumOfSquares(`data's`: IntArray, mean: Double): Double {
         var sumOfSquares = 0.0
         `data's`.forEach {
             sumOfSquares += (it - mean) * (it - mean)
         }
-
-        val variance = sumOfSquares / (`data's`.size - 1)
-
-
-        return "분산 : $variance"
-
-
+        return sumOfSquares
     }
+
+    private fun calculateMean(`data's`: IntArray): Double {
+        val sum = `data's`.sum()
+        return sum.toDouble() / `data's`.size
+    }
+
+
 }
